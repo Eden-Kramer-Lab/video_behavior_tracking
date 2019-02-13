a2f201b6f57af7fc66ad64b936642ae218cc74c2
Eric Denovellis <edeno@bu.edu>
2019-02-12 17:30:25 -0800

    Put some of the utilities on the top level to make it easier to import

--- video_behavior_tracking/__init__.py
+++ video_behavior_tracking/__init__.py
@@ -2,5 +2,6 @@
 from .kalman import extract_position_data
 from .labeled_video import make_video
 from .LED_detection import detect_LEDs
-from .utils import (convert_to_loren_frank_data_format, position_dataframe,
-                    save_loren_frank_data, video_filename_to_epoch_key)
+from .utils import (adjust_time, convert_to_loren_frank_data_format,
+                    position_dataframe, save_loren_frank_data,
+                    video_filename_to_epoch_key, write_config)
